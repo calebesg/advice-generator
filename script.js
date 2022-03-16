4c1ea8421008ece92b84f57e61afcc5c6a87b2c5
CalebeSG <calebeguimaraes10@gmail.com>
2022-03-16 12:20:10 -0300

    looking for advice in the api

---
+++ script.js
@@ -0,0 +1,28 @@
+const API = {
+  url: 'https://api.adviceslip.com/advice',
+
+  async getAdvice() {
+    const response = await fetch(API.url, { cach: 'no-cach' });
+    return response.json(); 
+  }
+}
+
+const DOM = {
+  title: document.querySelector('h2.title span'),
+  advice: document.querySelector('p.advice'),
+
+  addAdvice(data) {
+    const { id, advice } = data.slip;
+    
+    DOM.title.textContent = id;
+    DOM.advice.textContent = advice;
+  }
+}
+
+const Action = {
+  loadAdvice() {
+    API.getAdvice().then(DOM.addAdvice);
+  }
+}
+
+Action.loadAdvice();
